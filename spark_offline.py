2d5fc48a13f77075e2ab8ec33ae83f1c2798031d
Nils Kleine Klausing <nils.kleine.klausing@uni-duesseldorf.de>
2019-11-23 16:56:01 +0100

    made programs more modular

--- spark_offline.py
+++ spark_offline.py
@@ -1,43 +1,75 @@
-from pyspark.sql import SparkSession, Column
-from pyspark.sql.functions import explode, split, size, from_json, col, window, avg, max, min, count, date_format, when, \
-    row_number
-from pyspark.sql.types import StructType, StructField, TimestampType, StringType, DoubleType
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression
+from pyspark.sql import SparkSession, Column, DataFrame
+from pyspark.sql.functions import explode, split, size, from_json, col, window, avg, min, max, count, date_format, when
+from pyspark.sql.types import StructType, StructField, TimestampType, StringType, DoubleType, IntegerType, NumericType
 import os
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 ' \
-                                    'pyspark-shell'
 
-spark = SparkSession.builder.appName("TwitterSpark").getOrCreate()
+def analyze_offline_data():
+    os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 ' \
+                                        'pyspark-shell'
 
-schema = StructType([StructField("timestamp", TimestampType(), True),
-                     StructField("text", StringType(), True),
-                     StructField("sentiment", DoubleType(), True)])
+    spark = SparkSession.builder.appName("TwitterSparkBatch").getOrCreate()
 
-batch_df = spark\
-        .read\
-        .schema(schema)\
-        .format("json")\
-        .load(os.path.realpath("twitter_json"))
+    schema = StructType([StructField("timestamp", TimestampType(), True),
+                         StructField("text", StringType(), True),
+                         StructField("sentiment", DoubleType(), True)])
 
-windowed_batch_df = batch_df.withWatermark("timestamp", "10 seconds")\
-                    .groupBy(window("timestamp", "30 seconds"))\
-                    .agg(avg("sentiment").alias("avg_sentiment"),
-                         max("sentiment").alias("max_sentiment"),
-                         min("sentiment").alias("min_sentiment"),
-                         count(when(batch_df["sentiment"]>0.2, True)).alias("pos_tweet_count"),
-                         count(when((batch_df["sentiment"]<=0.2) & (batch_df["sentiment"]>=-0.2), True)).alias("neutral_tweet_count"),
-                         count(when(batch_df["sentiment"]<-0.2, True)).alias("neg_tweet_count"))
+    batch_df = spark\
+            .read\
+            .schema(schema)\
+            .format("json")\
+            .load(os.path.realpath("twitter_json"))
 
-windowed_batch_df = windowed_batch_df.select(windowed_batch_df.window.start.alias("start"),
-                                             windowed_batch_df.window.end.alias("end"),
-                                             "avg_sentiment",
-                                             "max_sentiment",
-                                             "min_sentiment",
-                                             "pos_tweet_count",
-                                             "neutral_tweet_count",
-                                             "neg_tweet_count")\
-                        .withColumn("start", date_format(col("start").cast(TimestampType()), "yyyy/MM/dd HH:mm:ss"))\
-                        .withColumn("end", date_format(col("end").cast(TimestampType()), "yyyy/MM/dd HH:mm:ss"))\
-                        .sort("start", ascending=True)
+    windowed_batch_df = batch_df.withWatermark("timestamp", "10 seconds")\
+                        .groupBy(window("timestamp", "30 seconds"))\
+                        .agg(avg("sentiment").alias("avg_sentiment"),
+                             max("sentiment").alias("max_sentiment"),
+                             min("sentiment").alias("min_sentiment"),
+                             count(when(batch_df["sentiment"]>0.1, True)).alias("pos_tweet_count_batch"),
+                             count(when((batch_df["sentiment"]<=0.1) & (batch_df["sentiment"]>=-0.1), True)).alias("neutral_tweet_count_batch"),
+                             count(when(batch_df["sentiment"]<-0.1, True)).alias("neg_tweet_count_batch"))
 
-batch_query = windowed_batch_df.show(50)
+    windowed_batch_df = windowed_batch_df.select(windowed_batch_df.window.start.alias("start"),
+                                                 "avg_sentiment",
+                                                 "max_sentiment",
+                                                 "min_sentiment",
+                                                 "pos_tweet_count_batch",
+                                                 "neutral_tweet_count_batch",
+                                                 "neg_tweet_count_batch").sort("start", ascending=False)
+                            #.withColumn("start", date_format(col("start").cast(TimestampType()), "yyyy/MM/dd HH:mm:ss"))\
+
+    windowed_batch_df.show(50)
+
+    return windowed_batch_df
+
+
+def train_regression_models(dataframe: DataFrame):
+    featAssembler = VectorAssembler(inputCols=["avg_sentiment", "max_sentiment", "min_sentiment"], outputCol="features")
+    dataframe = featAssembler.transform(dataframe)
+
+    lr1 = LinearRegression(labelCol="pos_tweet_count_batch", predictionCol="pos_tweet_count_pred", maxIter=10, regParam=0.3,
+                           elasticNetParam=0.8)
+    lrModel1 = lr1.fit(dataframe)
+
+    lr2 = LinearRegression(labelCol="neutral_tweet_count_batch", predictionCol="neutral_tweet_count_pred", maxIter=10,
+                           regParam=0.3, elasticNetParam=0.8)
+    lrModel2 = lr2.fit(dataframe)
+
+    lr3 = LinearRegression(labelCol="neg_tweet_count_batch", predictionCol="neg_tweet_count_pred", maxIter=10, regParam=0.3,
+                           elasticNetParam=0.8)
+    lrModel3 = lr3.fit(dataframe)
+
+    return [lrModel1, lrModel2, lrModel3]
+
+
+def get_batch_summary(dataframe: DataFrame):
+    summary_batch_df = dataframe.describe(["pos_tweet_count_batch", "neutral_tweet_count_batch", "neg_tweet_count_batch"])
+    summary_batch_df.show()
+    return summary_batch_df
+
+if __name__ == "__main__":
+    analyze_offline_data()
+
+
--- spark_streaming.py
+++ spark_streaming.py
@@ -1,113 +1,79 @@
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
-from pyspark.sql import SparkSession, Column
-from pyspark.sql.functions import explode, split, size, from_json, col, window, avg, min, max, count, date_format, when
+from pyspark.sql import SparkSession, Column, DataFrame
+from pyspark.sql.functions import from_json, col, window, avg, min, max, count, date_format, when, lit
 from pyspark.sql.types import StructType, StructField, TimestampType, StringType, DoubleType, IntegerType, NumericType
 import os
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 ' \
-                                    'pyspark-shell'
+def analyze_stream_data(lrModelList = None, batch_summary_df: DataFrame = None):
 
-spark = SparkSession.builder.appName("TwitterSpark").getOrCreate()
+    os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 ' \
+                                        'pyspark-shell'
+    spark = SparkSession.builder.appName("TwitterSparkStream").getOrCreate()
+    spark.conf.set("spark.sql.crossJoin.enabled", "true")
 
-schema = StructType([StructField("timestamp", TimestampType(), True),
-                     StructField("text", StringType(), True),
-                     StructField("sentiment", DoubleType(), True)])
+    schema = StructType([StructField("timestamp", TimestampType(), True),
+                         StructField("text", StringType(), True),
+                         StructField("sentiment", DoubleType(), True)])
 
-batch_df = spark\
-        .read\
-        .schema(schema)\
-        .format("json")\
-        .load(os.path.realpath("twitter_json"))
+    stream_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "tweet") \
+        .option("startingOffsets", "latest") \
+        .load() \
+        .select(from_json(col("value").cast("string"), schema).alias("data")) \
+        .select(col("data.timestamp").alias("timestamp"), col("data.text").alias("text"),
+                col("data.sentiment").alias("sentiment"))
 
-windowed_batch_df = batch_df.withWatermark("timestamp", "10 seconds")\
-                    .groupBy(window("timestamp", "30 seconds"))\
-                    .agg(avg("sentiment").alias("avg_sentiment"),
-                         max("sentiment").alias("max_sentiment"),
-                         min("sentiment").alias("min_sentiment"),
-                         count(when(batch_df["sentiment"]>0.1, True)).alias("pos_tweet_count"),
-                         count(when((batch_df["sentiment"]<=0.1) & (batch_df["sentiment"]>=-0.1), True)).alias("neutral_tweet_count"),
-                         count(when(batch_df["sentiment"]<-0.1, True)).alias("neg_tweet_count"))
+    windowed_stream_df = stream_df.withWatermark("timestamp", "10 seconds") \
+        .groupBy(window("timestamp", "30 seconds")) \
+        .agg(avg("sentiment").alias("avg_sentiment"),
+             max("sentiment").alias("max_sentiment"),
+             min("sentiment").alias("min_sentiment"),
+             count(when(stream_df["sentiment"] > 0.1, True)).alias("pos_tweet_count_stream"),
+             count(when((stream_df["sentiment"] <= 0.1) & (stream_df["sentiment"] >= -0.1), True)).alias(
+                 "neutral_tweet_count_stream"),
+             count(when(stream_df["sentiment"] < -0.1, True)).alias("neg_tweet_count_stream"))
 
-windowed_batch_df = windowed_batch_df.select(windowed_batch_df.window.start.alias("start"),
-                                             "avg_sentiment",
-                                             "max_sentiment",
-                                             "min_sentiment",
-                                             "pos_tweet_count",
-                                             "neutral_tweet_count",
-                                             "neg_tweet_count")\
-                        .withColumn("start_int", col("start").cast(IntegerType())) \
-                        .withColumn("start", date_format(col("start").cast(TimestampType()), "yyyy/MM/dd HH:mm:ss"))\
-                        .sort("start", ascending=True)\
-
-featAssembler = VectorAssembler(inputCols=["avg_sentiment", "max_sentiment", "min_sentiment"], outputCol="features")
-windowed_batch_df = featAssembler.transform(windowed_batch_df)
-
-lr1 = LinearRegression(labelCol="pos_tweet_count", predictionCol="pos_tweet_count_pred", maxIter=10, regParam=0.3, elasticNetParam=0.8)
-lrModel1 = lr1.fit(windowed_batch_df)
-
-lr2 = LinearRegression(labelCol="neutral_tweet_count", predictionCol="neutral_tweet_count_pred", maxIter=10, regParam=0.3, elasticNetParam=0.8)
-lrModel2 = lr2.fit(windowed_batch_df)
-
-lr3 = LinearRegression(labelCol="neg_tweet_count", predictionCol="neg_tweet_count_pred", maxIter=10, regParam=0.3, elasticNetParam=0.8)
-lrModel3 = lr3.fit(windowed_batch_df)
-
-windowed_batch_df.show(50)
+    windowed_stream_df = windowed_stream_df.select(windowed_stream_df.window.start.alias("start"),
+                                                   "avg_sentiment",
+                                                   "max_sentiment",
+                                                   "min_sentiment",
+                                                   "pos_tweet_count_stream",
+                                                   "neutral_tweet_count_stream",
+                                                   "neg_tweet_count_stream").sort("start", ascending=False)
+        #.withColumn("start", date_format(col("start").cast(TimestampType()), "yyyy/MM/dd HH:mm:ss"))\
 
 
-stream_df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "tweet") \
-    .option("startingOffsets", "latest") \
-    .load() \
-    .select(from_json(col("value").cast("string"), schema).alias("data")) \
-    .select(col("data.timestamp").alias("timestamp"), col("data.text").alias("text"),
-            col("data.sentiment").alias("sentiment"))
+    if lrModelList is not None:
+        featAssembler = VectorAssembler(inputCols=["avg_sentiment", "max_sentiment", "min_sentiment"], outputCol="features")
+        windowed_stream_df = featAssembler.transform(windowed_stream_df)
 
-windowed_stream_df = stream_df.withWatermark("timestamp", "10 seconds") \
-    .groupBy(window("timestamp", "30 seconds")) \
-    .agg(avg("sentiment").alias("avg_sentiment"),
-         max("sentiment").alias("max_sentiment"),
-         min("sentiment").alias("min_sentiment"),
-         count(when(stream_df["sentiment"] > 0.1, True)).alias("pos_tweet_count"),
-         count(when((stream_df["sentiment"] <= 0.1) & (stream_df["sentiment"] >= -0.1), True)).alias(
-             "neutral_tweet_count"),
-         count(when(stream_df["sentiment"] < -0.1, True)).alias("neg_tweet_count"))
+        windowed_stream_df = lrModelList[0].transform(windowed_stream_df)
+        windowed_stream_df = lrModelList[1].transform(windowed_stream_df)
+        windowed_stream_df = lrModelList[2].transform(windowed_stream_df)
 
-windowed_stream_df = windowed_stream_df.select(windowed_stream_df.window.start.alias("start"),
-                                               "avg_sentiment",
-                                               "max_sentiment",
-                                               "min_sentiment",
-                                               "pos_tweet_count",
-                                               "neutral_tweet_count",
-                                               "neg_tweet_count") \
-    .withColumn("start_int", col("start").cast(IntegerType())) \
-    .withColumn("start", date_format(col("start").cast(TimestampType()), "yyyy/MM/dd HH:mm:ss"))\
-    .sort("start", ascending=False)
+        windowed_stream_df = windowed_stream_df.select("start", "avg_sentiment", "max_sentiment", "min_sentiment",
+                    "pos_tweet_count_stream", "pos_tweet_count_pred",
+                    "neutral_tweet_count_stream", "neutral_tweet_count_pred",
+                    "neg_tweet_count_stream", "neg_tweet_count_pred")
 
-featAssembler = VectorAssembler(inputCols=["avg_sentiment", "max_sentiment", "min_sentiment"], outputCol="features")
-windowed_stream_df = featAssembler.transform(windowed_stream_df)
-
-windowed_stream_df = lrModel1.transform(windowed_stream_df)
-windowed_stream_df = lrModel2.transform(windowed_stream_df)
-windowed_stream_df = lrModel3.transform(windowed_stream_df)
-
-windowed_stream_df = windowed_stream_df.withColumn("pos_tweet_error", col("pos_tweet_count") - col("pos_tweet_count_pred"))\
-                    .withColumn("neutral_tweet_error", col("neutral_tweet_count") - col("neutral_tweet_count_pred"))\
-                    .withColumn("neg_tweet_error", col("neg_tweet_count") - col("neg_tweet_count_pred"))\
-                    .select("start", "avg_sentiment", "max_sentiment", "min_sentiment",
-                            "pos_tweet_count", "pos_tweet_count_pred", "pos_tweet_error",
-                            "neutral_tweet_count", "neutral_tweet_count_pred", "neutral_tweet_error",
-                            "neg_tweet_count", "neg_tweet_count_pred", "neg_tweet_error")
+    if batch_summary_df is not None:
+        windowed_stream_df = windowed_stream_df.withColumn("tmp1", lit("mean"))
+        windowed_stream_df = windowed_stream_df.alias("stream").join(batch_summary_df.alias("batch"),
+                                                     col("stream.tmp1") == col("batch.summary"), "inner")
 
 
-query = windowed_stream_df.writeStream\
+    query = windowed_stream_df.writeStream\
                             .format("console")\
                             .outputMode("complete")\
                             .option("truncate", False)\
                             .start().awaitTermination()
+
+if __name__ == "__main__":
+    analyze_stream_data()
 
 
 
