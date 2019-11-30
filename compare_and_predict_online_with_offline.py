042f32627a175d7724f214fb48d0b88a86a0cb7c
Nils Kleine Klausing <nils.kleine.klausing@uni-duesseldorf.de>
2019-11-30 20:09:29 +0100

    added commentary

--- compare_and_predict_online_with_offline.py
+++ compare_and_predict_online_with_offline.py
@@ -1,8 +1,14 @@
 import spark_offline
 import spark_streaming
 
+# analyze historic data
 batch_df = spark_offline.analyze_offline_data()
+
+# train LinearRegression Models
 lrModelList = spark_offline.train_regression_models(batch_df)
+
+# calculate average of metrics
 batch_summary_df = spark_offline.get_batch_summary(batch_df)
 
+# compare and predict realtime data with historic data
 spark_streaming.analyze_stream_data(lrModelList=lrModelList, batch_summary_df=batch_summary_df)
